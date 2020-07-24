2da2372d388a25babeb6839e77622312bc938e4b
roberto890 <roberto.jesus.amaral@gmail.com>
2020-07-24 11:25:06 +0000

    initial commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,21 +1,13 @@
 package application;
 
 import java.util.List;
-import java.util.ArrayList;
-import entities.Product;
+import java.util.Arrays;
 
 public class Program {
 
     public static void main(final String[] args) {
 
-        final List<Product> list = new ArrayList<>();
-        list.add(new Product("Tv", 900.00));
-        list.add(new Product("Mouse", 50.00));
-        list.add(new Product("Tablet", 350.50));
-        list.add(new Product("HD Case", 80.90));
-
-
-        System.out.println("Sum = " + String.format("%.2f", sum));
+        final List<Integer> list = Arrays.asList(3,4,5,10,7);
         
     }
 }
